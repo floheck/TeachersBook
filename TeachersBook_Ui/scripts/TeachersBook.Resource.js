187e9f90d03799cf07e9b252c4675031422c7069
floheck <florian.heck@googlemail.com>
2017-06-08 20:21:57 +0200

    * TeachersBook.Configuration.js: Drag & Drop implementiert
    * index.html:
    * index.js:
    * homepage.css:
    * navigation.js:
    * navigation.css:
    * dragAndDrop.js:
    * TeachersBook_Ui.csproj:
    * bewerten.jpg:
    * stundenplan1.jpg:
    * Timetable.js:
    * TeachersBook.Resource.js:
    * Timetable.css:
    * Timetable.html:
    * custom.js:
    * Timetable.json:
    * custom.css:
    * TeachersBook.ModuleLoader.js:
    * index.html:
    * custom.min.js:
    * LICENSE.txt:
    * font-awesome.css:
    * AUTHORS.txt:
    * jquery-ui.js:
    * knockout-sortable.js:
    * custom.min.css:
    * package.json:
    * jquery-ui.css:
    * font-awesome.css:
    * font-awesome.min.css:
    * FontAwesome.otf:
    * jquery-ui.min.js:
    * jquery-ui.min.css:
    * jquery-ui.theme.css:
    * font-awesome.min.css:
    * jquery-ui.structure.css:
    * jquery-ui.theme.min.css:
    * fontawesome-webfont.ttf:
    * fontawesome-webfont.eot:
    * fontawesome-webfont.svg:
    * fontawesome-webfont.woff:
    * jquery.js:
    * fontawesome-webfont.woff2:
    * jquery-ui.structure.min.css:
    * ui-icons_444444_256x240.png:
    * ui-icons_555555_256x240.png:
    * ui-icons_777620_256x240.png:
    * ui-icons_777777_256x240.png:
    * ui-icons_cc0000_256x240.png:
    * ui-icons_ffffff_256x240.png:

---
+++ TeachersBook_Ui/scripts/TeachersBook.Resource.js
@@ -0,0 +1,68 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+
+"use strict";
+(function (TeachersBook) {
+        (function (Resource) {
+
+            Resource.init = function (data_link, defaultLanguage) {
+
+                Resource.defaultLanguage = defaultLanguage;
+
+                /// <summary>
+                /// Initialisiert eine Language Resource
+                /// </summary>
+                /// <param name="data_link" type="type">Pfad zum Resource-File</param>
+                /// <returns type="jQuery.Deferred">Deferred</returns>
+                var deferred = new jQuery.Deferred();
+                var readResourceFile = jQuery.ajax({
+                    url: data_link,
+                    dataType: "json",
+                    cache: false
+                });
+                 
+                readResourceFile.done(function (data) {
+                    
+                    var LanguageResource = {};
+                    var currentLanguage = Resource.getLanguage(Resource.defaultLanguage);
+
+                    for (var language in data) {
+                        if (language == currentLanguage) {
+                            LanguageResource = data[language];
+                        }
+                    }
+                    if (!LanguageResource) {
+                        LanguageResource = data["en"];
+                    }
+                    deferred.resolve(LanguageResource);
+                });
+
+                readResourceFile.fail(function (jqxhr, settings, exception) {
+                    deferred.reject(jqxhr, settings, exception);
+                });
+                return deferred.promise();
+            }
+
+
+            Resource.getLanguage = function (defaultLanguage) {
+                /// <summary>
+                /// Ermittelt ob eine Language Resource
+                /// </summary>
+                /// <param name="defaultLanguage" type="type">Der Language code (z.B. 'de') oder 'browser' wenn die Browsersprache genutzt werden soll.</param>
+                /// <returns type="json">Language Resource Objekt</returns>
+                var language;
+                if (!defaultLanguage || defaultLanguage.toLowerCase() == "browser") {
+                    var myNav = navigator.userAgent.toLowerCase();
+                    var msieVersion = (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : -1;
+                    language = msieVersion != -1 ? navigator.browserLanguage : language = navigator.language;
+                }
+                else {
+                    language = defaultLanguage;
+                }                
+
+                language = language.replace(/-.*/, "");
+
+                return language;
+            }
+
+        }(TeachersBook.Resource = TeachersBook.Resource || {}));
+}(window.TeachersBook = window.TeachersBook || {}));
---
+++ TeachersBook_Ui/sections/timetable/Timetable.js
@@ -0,0 +1,105 @@
+﻿(function (TeachersBook) {
+	(function (Section) {
+		(function (Timetable) {
+
+			Timetable.ViewModel = function (configuration) {
+
+				var viewModel = this;
+
+				viewModel.drag_start_index = ko.observable();
+			        viewModel.drag_target_index = ko.observable();
+			        viewModel.dragging = ko.computed(function() {
+			            return viewModel.drag_start_index() >= 0;
+			        });
+			        viewModel.dragItem = ko.observable();
+
+				viewModel.subjects = ko.observableArray([
+					{name : "Mathe", class: "1A", color : "#e86f6f"}, 
+					{name : "Deutsch", class: "3C", color : "#98e299"}
+			  	]);
+
+			  	viewModel.replaceSubjectInTimetable = function (data, event) {
+		  		    viewModel.timetable()[data.hour].subjects()[data.day].content.name(viewModel.dragItem().name);
+		  		    viewModel.timetable()[data.hour].subjects()[data.day].content.color(viewModel.dragItem().color);
+			  	}
+
+			  	viewModel.swap = function(from, to) {
+			  	debugger;
+			           if (to > viewModel.subjects().length - 1 || to < 0) return;
+
+			            var fromObj = viewModel.subjects()[from];
+			            var toObj = viewModel.subjects()[to];
+			            viewModel.subjects()[to] = fromObj;
+			            viewModel.subjects()[from] = toObj;
+			            viewModel.subjects.valueHasMutated()
+			        }
+
+				viewModel.timetable = ko.observableArray([
+					{subjects : ko.observableArray([
+						{hour: 0, day: 0, content: {name : "7:30 - 8:15", color: "transparent"}},
+						{hour: 0, day: 1, content: {name : new ko.observable(""), class : ko.observable(""), color : ko.observable("")}},
+						{hour: 0, day: 2, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 0, day: 3, content: {name : new ko.observable("Mathe"), class: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 0, day: 4, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 0, day: 5, content: {name : new ko.observable("Mathe"), class: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
+					])},
+					{subjects : ko.observableArray([
+						{hour: 1, day: 0, content: {name : "8:20 - 9:05", color: "transparent"}},
+						{hour: 1, day: 1, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 1, day: 2, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 1, day: 3, content: {name : new ko.observable("Mathe"), class: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 1, day: 4, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 1, day: 5, content: {name : new ko.observable("Mathe"), class: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
+					])},
+					{subjects : ko.observableArray([
+						{hour: 2, day: 0, content: {name : "9:25 - 10:10", color: "transparent"}},
+						{hour: 2, day: 1, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 2, day: 2, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 2, day: 3, content: {name : new ko.observable("Mathe"), class: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 2, day: 4, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 2, day: 5, content: {name : new ko.observable("Mathe"), class: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
+					])},
+					{subjects : ko.observableArray([
+						{hour: 3, day: 0, content: {name : "10:15 - 11:00", color: "transparent"}},
+						{hour: 3, day: 1, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 3, day: 2, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 3, day: 3, content: {name : new ko.observable("Mathe"), class: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 3, day: 4, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 3, day: 5, content: {name : new ko.observable("Mathe"), class: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
+					])},
+					{subjects : ko.observableArray([
+						{hour: 4, day: 0, content: {name : "11:10 - 11:55", color: "transparent"}},
+						{hour: 4, day: 1, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 4, day: 2, content: {name : new ko.observable("Deutsch"), class: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 4, day: 3, content: {name : new ko.observable("Mathe"), class: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 4, day: 4, content: {name : "Deutsch", class: "3C", color : "#98e299"}},
+						{hour: 4, day: 5, content: {name : new ko.observable(""), class : new ko.observable(""), color : new ko.observable("")}}
+					])}
+				]);
+			}
+
+			ko.bindingHandlers.flash = {
+			    init: function(element) {
+			        $(element).hide();
+			    },
+			    update: function(element, valueAccessor) {
+			        var value = ko.utils.unwrapObservable(valueAccessor());
+			        if (value) {
+			            $(element).stop().hide().text(value).fadeIn(function() {
+			                clearTimeout($(element).data("timeout"));
+			                $(element).data("timeout", setTimeout(function() {
+			                    $(element).fadeOut();
+			                    valueAccessor()(null);
+			                }, 3000));
+			            });
+			        }
+			    },
+			    timeout: null
+			};
+
+			//ko.bindingHandlers.sortable.beforeMove = vm.verifyAssignment;
+			//ko.bindingHandlers.sortable.afterMove = vm.updateLastAction;
+
+		}(Section.Timetable = Section.Timetable || {}));
+	}(TeachersBook.Section = TeachersBook.Section || {}));
+}(window.TeachersBook = window.TeachersBook || {}));
