d8ac8b5a2403c64056a63f1d00c801fe2a6d1a90
floheck <florian.heck@googlemail.com>
2017-06-28 19:41:21 +0200

    Designanpassungen Stundenplanelement Löschen von  Stunden aus dem Stundenplan

--- TeachersBook_Ui/sections/timetable/Timetable.js
+++ TeachersBook_Ui/sections/timetable/Timetable.js
@@ -64,6 +64,8 @@
 			  	viewModel.replaceSubjectInTimetable = function (data, event) {
 		  		    viewModel.timetable()[data.hour].subjects()[data.day].content.name(viewModel.dragItem().name);
 		  		    viewModel.timetable()[data.hour].subjects()[data.day].content.color(viewModel.dragItem().color);
+                    viewModel.timetable()[data.hour].subjects()[data.day].content.schoolGrade(viewModel.dragItem().schoolGrade);
+                    viewModel.timetable()[data.hour].subjects()[data.day].hasSubject(true);
 			  	}
 
                 viewModel.openAddNewSubjectDialog = function () {
@@ -80,29 +82,38 @@
                     }
                 }
 
+                viewModel.deleteSubject = function(data, event) {
+                    viewModel.timetable()[data.hour].subjects()[data.day].content.name("&nbsp;");
+                    viewModel.timetable()[data.hour].subjects()[data.day].content.color("");
+                    viewModel.timetable()[data.hour].subjects()[data.day].content.schoolGrade("");
+                    viewModel.timetable()[data.hour].subjects()[data.day].hasSubject(false);
+                    jQuery(".timetable-remove-subject").style("opacity", "0");
+                }
+
                 viewModel.subjects = ko.observableArray([
                     {name : "Mathe", schoolGrade: "1A", color : "#e86f6f"}, 
-                    {name : "Deutsch", schoolGrade: "3C", color : "#98e299"}
+                    {name : "Deutsch", schoolGrade: "3C", color : "#98e299"}, 
+                    {name : "Langer Test wegen Umbruch oder so", schoolGrade: "3C", color : "#98e299"}
                 ]);
                 
 				viewModel.timetable = ko.observableArray([
 					{rowtype: "subjects",
                      subjects : ko.observableArray([
-						{hour: 0, day: 0, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("7:30 - 8:15"), color: "transparent"}},
-						{hour: 0, day: 1, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("&nbsp;"), schoolGrade : ko.observable(""), color : ko.observable("")}},
-						{hour: 0, day: 2, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 0, day: 3, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
-						{hour: 0, day: 4, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 0, day: 5, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
+						{hour: 0, day: 0, hasSubject: ko.observable(false), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("7:30 - 8:15"), color: "transparent"}},
+						{hour: 0, day: 1, hasSubject: ko.observable(false), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("&nbsp;"), schoolGrade : ko.observable(""), color : ko.observable("")}},
+						{hour: 0, day: 2, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 0, day: 3, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 0, day: 4, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 0, day: 5, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
 					])},
 					{rowtype: "subjects",
                      subjects : ko.observableArray([
-						{hour: 1, day: 0, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("8:20 - 9:05"), color: "transparent"}},
-						{hour: 1, day: 1, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 1, day: 2, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 1, day: 3, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
-						{hour: 1, day: 4, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 1, day: 5, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
+						{hour: 1, day: 0, hasSubject: ko.observable(false), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("8:20 - 9:05"), color: "transparent"}},
+						{hour: 1, day: 1, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 1, day: 2, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 1, day: 3, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 1, day: 4, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 1, day: 5, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
 					])},
                     {rowtype: "break",
                      subjects : ko.observableArray([
@@ -115,30 +126,30 @@
                     ])},
 					{rowtype: "subjects",
                      subjects : ko.observableArray([
-						{hour: 3, day: 0, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("9:25 - 10:10"), color: "transparent"}},
-						{hour: 3, day: 1, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 3, day: 2, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 3, day: 3, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
-						{hour: 3, day: 4, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 3, day: 5, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
+						{hour: 3, day: 0, hasSubject: ko.observable(false), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("9:25 - 10:10"), color: "transparent"}},
+						{hour: 3, day: 1, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 3, day: 2, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 3, day: 3, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 3, day: 4, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 3, day: 5, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
 					])},
 					{rowtype: "subjects",
                      subjects : ko.observableArray([
-						{hour: 4, day: 0, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("10:15 - 11:00"), color: "transparent"}},
-						{hour: 4, day: 1, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 4, day: 2, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 4, day: 3, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
-						{hour: 4, day: 4, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 4, day: 5, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
+						{hour: 4, day: 0, hasSubject: ko.observable(false), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("10:15 - 11:00"), color: "transparent"}},
+						{hour: 4, day: 1, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 4, day: 2, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 4, day: 3, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 4, day: 4, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 4, day: 5, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}}
 					])},
 					{rowtype: "subjects",
                      subjects : ko.observableArray([
-						{hour: 5, day: 0, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("11:10 - 11:55"), color: "transparent"}},
-						{hour: 5, day: 1, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 5, day: 2, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 5, day: 3, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
-						{hour: 5, day: 4, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
-						{hour: 5, day: 5, labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("&nbsp;"), schoolGrade : new ko.observable(""), color : new ko.observable("")}}
+						{hour: 5, day: 0, hasSubject: ko.observable(false), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("11:10 - 11:55"), color: "transparent"}},
+						{hour: 5, day: 1, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 5, day: 2, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 5, day: 3, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Mathe"), schoolGrade: new ko.observable("1A"), color : new ko.observable("#e86f6f")}},
+						{hour: 5, day: 4, hasSubject: ko.observable(true), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("Deutsch"), schoolGrade: new ko.observable("3C"), color : new ko.observable("#98e299")}},
+						{hour: 5, day: 5, hasSubject: ko.observable(false), labelVisible: ko.observable(true), inlineEditingVisible: ko.observable(false), content: {name : ko.observable("&nbsp;"), schoolGrade : new ko.observable(""), color : new ko.observable("")}}
 					])}
 				]);
 			}
